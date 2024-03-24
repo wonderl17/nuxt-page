059a9b34c472c9789f9ecbb033bc17f920fe0660
wonderl17 <809646531@qq.com>
2024-03-24 20:54:10 +0800

    add active status

--- components/Corona.vue
+++ components/Corona.vue
@@ -13,14 +13,24 @@
         whitespace-nowrap
         transform-origin-left
         text-50px
+        color="#979797"
         :style="{
-          transform: `translateY(${
-            Math.sin(angleStep * getOffset(index)) * radius + 300
-          }px) translateX(${
-            Math.cos(angleStep * getOffset(index)) * radius - 75
-          }px) rotate(${(angleStep * getOffset(index) * 180) / Math.PI}deg)`,
+          ...getTransformStyle(index),
+          color: active === index ? '#000' : '#979797',
         }"
       >
+        <span
+          v-show="active === index"
+          bg="#ff5f2c"
+          w-20px
+          h-20px
+          b-rd="50%"
+          overflow-hidden
+          absolute
+          top="50%"
+          translate-y--50%
+          left--50px
+        ></span>
         {{ item }}
       </div>
     </div>
@@ -34,6 +44,31 @@ const items = ref(new Array(12).fill("Make a song for my friends"));
 const angleStep = ((120 / items.value.length) * Math.PI) / 180;
 const radius = ref(500);
 const scrollY = ref(0);
+
+const offsetMap = ref(new Map());
+
+const getTransformStyle = (index: number) => {
+  const offset = getOffset(index);
+  offsetMap.value.set(index, offset);
+
+  return {
+    transform: `translateY(${
+      Math.sin(angleStep * offset) * radius.value + 300
+    }px) translateX(${
+      Math.cos(angleStep * offset) * radius.value - 75
+    }px) rotate(${(angleStep * offset * 180) / Math.PI}deg)`,
+  };
+};
+
+const active = computed(() => {
+  const min = Array.from(offsetMap.value.entries()).sort((a, b) => {
+    return Math.abs(a[1]) - Math.abs(b[1]);
+  })[0];
+
+  if (Math.abs(angleStep * min[1] * 180) / Math.PI > 5) return -1;
+
+  return min[0];
+});
 
 const getOffset = (index: number) => {
   return -2 + index - Math.min(y.value, 250) * 0.04;
