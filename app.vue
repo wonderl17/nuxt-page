8b2429434998946410131f9109016e3728bf64e7
wonderl17 <809646531@qq.com>
2024-03-25 21:41:25 +0800

    fix style

--- app.vue
+++ app.vue
@@ -9,3 +9,10 @@
     <div h-1500px></div>
   </NuxtLayout>
 </template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
--- components/Corona.vue
+++ components/Corona.vue
@@ -1,8 +1,6 @@
 <template>
   <div relative>
-    corona
-    {{ { x, y, isScrolling, arrivedState, directions } }}
-    <div relative left="-30%" :style="{}">
+    <div relative left="-300px">
       <div
         v-for="(item, index) in items"
         class="item"
@@ -12,12 +10,15 @@
         left="0"
         whitespace-nowrap
         transform-origin-left
-        text-50px
+        text-75px
         color="#979797"
+        :class="{ active: active === index }"
+        hover-c="#666"
+        cursor-pointer
         :style="{
           ...getTransformStyle(index),
-          color: active === index ? '#000' : '#979797',
         }"
+        @click="active = index"
       >
         <span
           v-show="active === index"
@@ -39,11 +40,11 @@
 
 <script lang="ts" setup>
 import { useScroll } from "@vueuse/core";
+const { x, y, isScrolling, arrivedState, directions } = useScroll(window);
 
 const items = ref(new Array(12).fill("Make a song for my friends"));
-const angleStep = ((120 / items.value.length) * Math.PI) / 180;
+const angleStep = ((150 / items.value.length) * Math.PI) / 180;
 const radius = ref(500);
-const scrollY = ref(0);
 
 const offsetMap = ref(new Map());
 
@@ -60,7 +61,8 @@ const getTransformStyle = (index: number) => {
   };
 };
 
-const active = computed(() => {
+const active = ref(-1);
+const getActive = () => {
   const min = Array.from(offsetMap.value.entries()).sort((a, b) => {
     return Math.abs(a[1]) - Math.abs(b[1]);
   })[0];
@@ -68,16 +70,24 @@ const active = computed(() => {
   if (Math.abs(angleStep * min[1] * 180) / Math.PI > 5) return -1;
 
   return min[0];
-});
+};
 
 const getOffset = (index: number) => {
   return -2 + index - Math.min(y.value, 250) * 0.04;
 };
 
-const { x, y, isScrolling, arrivedState, directions } = useScroll(window);
+const updateActive = () => {
+  active.value = getActive();
+};
+
+watch(y, updateActive);
+onMounted(() => {
+  updateActive();
+});
 </script>
 
 <style lang="scss" scoped>
-.item {
+.item.active {
+  color: #000;
 }
 </style>
